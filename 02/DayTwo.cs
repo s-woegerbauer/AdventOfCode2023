23d264ea9e104292e61f9cf3bc49d2f910490937
s-woegerbauer <simon.woeger@gmail.com>
2023-12-02 08:32:56 +0100

    Day 2 Fix
    I accidentaly put both solutions into part one, fixed it now :grin:

--- 02/DayTwo.cs
+++ 02/DayTwo.cs
@@ -10,8 +10,8 @@ namespace AdventOfCode2023
             string[] input = InputOutputHelper.GetInput(false, "02");
             PartOne(false, input);
 
-            //PartTwo(true, testInput);
-            //PartTwo(false, input);
+            PartTwo(true, testInput);
+            PartTwo(false, input);
         }
 
         public static void PartOne(bool isTest, string[] input)
@@ -50,7 +50,12 @@ namespace AdventOfCode2023
                     revealed["green"] = Math.Max(revealed["green"], current["green"]);
                 }
 
-                result += revealed["blue"] * revealed["red"] * revealed["green"];
+                if (revealed["blue"] <= 14 && revealed["green"] <= 13 && revealed["red"] <= 12)
+                {
+                    result += id;
+                }
+
+                id++;
             }
 
             InputOutputHelper.WriteOutput<int>(isTest, result);
@@ -59,6 +64,41 @@ namespace AdventOfCode2023
         public static void PartTwo(bool isTest, string[] input)
         {
             int result = 0;
+            int id = 1;
+
+            foreach (string line in input)
+            {
+                string[] splitted = line.Split(": ")[1].Split("; ");
+                Dictionary<string, int> revealed = new Dictionary<string, int>();
+                revealed.Add("blue", 0);
+                revealed.Add("red", 0);
+                revealed.Add("green", 0);
+
+                for (int i = 0; i < splitted.Length; i++)
+                {
+                    Dictionary<string, int> current = new Dictionary<string, int>();
+                    current.Add("blue", 0);
+                    current.Add("red", 0);
+                    current.Add("green", 0);
+                    string[] currentRevealed = splitted[i].Split(", ");
+
+                    foreach (string rev in currentRevealed)
+                    {
+                        if (!rev.StartsWith("Game"))
+                        {
+                            string color = rev.Split(' ')[1];
+                            int amount = int.Parse(rev.Split(' ')[0]);
+                            current[color] = amount;
+                        }
+                    }
+
+                    revealed["blue"] = Math.Max(revealed["blue"], current["blue"]);
+                    revealed["red"] = Math.Max(revealed["red"], current["red"]);
+                    revealed["green"] = Math.Max(revealed["green"], current["green"]);
+                }
+
+                result += revealed["blue"] * revealed["red"] * revealed["green"];
+            }
 
 
             InputOutputHelper.WriteOutput<int>(isTest, result);
